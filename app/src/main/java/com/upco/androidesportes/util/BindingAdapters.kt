969a8e53e63fb79202f36d2d4ffe0b20d4e48b20
Felipe Maciel <mrchampz@outlook.com>
2019-04-27 18:21:43 -0300

    Implementados os BindingAdapters e Handlers, utilizados em conjunto com o DataBinding, para que seja possível passar os valores dinâmicos a partir do código estático, e fazer o tratamento desses valores em código dinâmico

---
+++ app/src/main/java/com/upco/androidesportes/util/BindingAdapters.kt
@@ -0,0 +1,42 @@
+package com.upco.androidesportes.util
+
+import android.widget.ImageView
+import androidx.databinding.BindingAdapter
+import androidx.swiperefreshlayout.widget.CircularProgressDrawable
+import com.squareup.picasso.Picasso
+import com.upco.androidesportes.R
+
+/**
+ * BindingAdapters são funções que extendem a classe a qual se referem, de modo
+ * que seja possível passar valores, no código estático, por meio do DataBinding
+ * para a classe referida.
+ * <p>
+ * Por exemplo, definimos um BindingAdapter que possibilita que qualquer
+ * ImageView possa ser configurada e tenha sua imagem carrega por meio da url
+ * (passada através do atributo app:imageUrl).
+ */
+
+/**
+ * BindingAdapter que configura um ImageView e carrega sua imagem a partir da url,
+ * passada através do atributo app:imageUrl.
+ */
+@BindingAdapter("app:imageUrl")
+fun ImageView.setImageUrl(url: String) {
+    //
+    val progress = CircularProgressDrawable(context)
+    progress.apply {
+        setColorSchemeColors(resources.getColor(R.color.colorPrimary))
+        strokeWidth = 5f
+        centerRadius = 30f
+        start()
+    }
+
+    Picasso.get()
+           .load(url)
+           .fit()
+           .noFade()
+           .centerCrop()
+           .placeholder(progress)
+           .error(R.drawable.iv_error_placeholder)
+           .into(this)
+}
---
+++ app/src/main/java/com/upco/androidesportes/util/BindingHandlers.kt
@@ -0,0 +1,19 @@
+package com.upco.androidesportes.util
+
+import android.app.Activity
+import android.widget.Toast
+import com.upco.androidesportes.model.News
+
+/**
+ * Classe que fornece métodos handler, que são chamados a partir de alguma ação na UI.
+ */
+class BindingHandlers(val activity: Activity) {
+
+    /**
+     * Handler chamado ao clicar em um item (notícia) no RecyclerView.
+     */
+    fun onClickNews(news: News) {
+        Toast.makeText(activity, "Notícia", Toast.LENGTH_SHORT)
+             .show()
+    }
+}
